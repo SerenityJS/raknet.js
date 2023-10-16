d263b46d368de762a6b9a258df05c46e15eb29c0
PMK744 <69542280+PMK744@users.noreply.github.com>
2023-10-15 20:39:52 -0500

    exports

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,4 @@
-import { Raknet } from './Raknet'
-import { RaknetEvent } from './constants'
-import { BinaryStream } from 'binarystream.js'
-
-const server = new Raknet(618, '1.20.32')
-
-server.listen('127.0.0.1', 19132)
-
-server.on(RaknetEvent.Listening, () => {
-  console.log(`Listening on ${server.socket.address().address}:${server.socket.address().port}`)
-})
-
-server.on(RaknetEvent.GamePacket, (bin, size, connection) => {
-  const stream = new BinaryStream(bin)
-  const id = stream.readUInt8()
-})
+export * from './Raknet'
+export * from './packets'
+export * from './connection'
+export * from './constants'
